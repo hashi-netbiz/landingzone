9d24aa66eaca4896f0483854cca3499dc03c5a91
hashi-netbiz <rachelngunjoh@gmail.com>
2023-09-05 03:24:05 -0400

    sassaigyunnbnwefwdwdsassd

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -77,10 +77,10 @@ resource "azurerm_monitor_diagnostic_setting" "settings" {
     category = "kube-apiserver"
     enabled  = true
 
-    retention_policy {
-      enabled = true  
-      days    = var.log_analytics_retention_days
-    }
+    # retention_policy {
+    #   enabled = true  
+    #   days    = var.log_analytics_retention_days
+    # }
   }
 
   log {
